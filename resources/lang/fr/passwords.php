12dda291ef082ca31c0b727f8a74166446e8ad64
Lendarid <ber.jxf@gmail.com>
2018-05-29 11:15:44 +0200

    Traduction fr partie 1

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Votre Mot de Passe doit contenir au moins 6 caractères et correspondre à la confirmation.',
+    'reset' => 'Votre Mot de Passe est réinitialisé !',
+    'sent' => 'Un e-mail vous à été envoyé avec un lien pour reinitialiser votre Mot de Passe !',
+    'token' => 'Ce token de réinitialisation est erroné !',
+    'user' => "Nous ne pouvons trouver d'utilisateur correspondant à cette adresse e-mail.",
+
+];
